95d5689cb78d19d4a098ce294c2b8404457a3ba6
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-21 16:48:20 +0100

    finsihed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const app = express();
 
-// Reruire dateMaker
+// Require dateMaker
 const dateMaker = require('./dateMaker');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
